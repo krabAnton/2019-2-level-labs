0e58ec8739e82de9a04b36cc3c7313d0cb508c81
krabAnton <55597210+krabAnton@users.noreply.github.com>
2019-10-06 09:15:35 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,20 +5,92 @@ Count frequencies dictionary by the given arbitrary text
 
 
 def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
+
+    if type(text) != str:
+
+        return {} 
+
+    frequencies = {} 
+
+    text = text.lower() 
+
+    illigal_signs = '1234567890^%~|[]<>{}@#&*-+=()_₽$€¥"£¢:;/?!`÷×§¶°¬¡¿™®©'
+
+    for i in text:
+
+        if i in illigal_signs:
+
+            text = text.replace(i,'')
+
+    text = text.split() 
+
+    for i in text:
+
+        frequencies[i] = 0
+
+    for i in frequencies:
+
+        for k in text:
+
+            if i == k:
+
+               frequencies[i] = frequencies.get(i, 0) + 1              
+
+    return frequencies
+
+frequencies = calculate (text) 
+    
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
+    if type(frequencies)!= dict and type(stop_words) != tuple:
+
+        return {} 
+
+    d_clean = {} 
+
+    for k, v in frequencies.items():
+
+        if k not in stop_words and type(k) == str:
+
+            d_clean[k] = v 
+
+    return d_clean  
+
+filter_stop_words (frequencies, stop_words)
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
-Print
+    "if type(frequencies) != dict or type(top_n) != int:
+
+        return () 
+
+    answer = () 
+
+    if len(frequencies) < top_n:
+
+        print ('В словаре только', l, 'записей') 
+
+    freqsorted = sorted(frequencies.items(), key=lambda item: item[1], reverse = True)
+
+    d_cleansort = {} 
+
+    for k, v in freqsorted:
+
+        d_cleansort[k] = v   
+
+    m = 0
+
+    for k, v in d_cleansort.items():
+
+        if m != top_n:
+
+            answer += (k, ) 
+
+        else:
+
+            break
+
+        m += 1
+
+    return answer 
+
+get_top_n (frequencies, top_n) 
