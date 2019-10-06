39914a7f536cf5114e96610aeb015eb0f5972a20
krabAnton <krabanton@gmail.com>
2019-10-06 17:25:09 +0300

    final

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,93 +5,52 @@ Count frequencies dictionary by the given arbitrary text
 
 
 def calculate_frequences(text: str) -> dict:
-
     if type(text) != str:
-
-        return {} 
-
-    frequencies = {} 
-
-    text = text.lower() 
-
-    illigal_signs = '1234567890^%~|[]<>{}@#&*-+=()_₽$€¥"£¢:;/?!`÷×§¶°¬¡¿™®©'
-
+        return {}
+    frequencies = {}
+    text = text.lower()
+    text = text.replace('\n', ' ')
+    illigal_signs = '1234567890^%~|[]<>{}@#&*-+=()_₽$€¥£¢:;/?!`÷×§¶°¬¡¿™®©'
     for i in text:
-
-        if i in illigal_signs:
-
-            text = text.replace(i,'')
-
-    text = text.split() 
-
+        if i in illigal_signs or i.isalpha() == False and i != ' ':
+            text = text.replace(i, '')
+    text = text.split()
     for i in text:
-
         frequencies[i] = 0
-
     for i in frequencies:
-
         for k in text:
-
             if i == k:
-
-               frequencies[i] = frequencies.get(i, 0) + 1              
-
+               frequencies[i] = frequencies.get(i, 0) + 1
     return frequencies
 
+def filter_stop_words(frequencies: dict, stop_words: tuple):
+    if type(frequencies) != dict or type(stop_words) != tuple:
+        return {}
+    else:
+        d_clean = {}
+        for k, v in frequencies.items():
+            if k not in stop_words and type(k) == str:
+                d_clean[k] = v
+        return d_clean
 
-    
-
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    if type(frequencies)!= dict and type(stop_words) != tuple:
-
-        return {} 
-
-    d_clean = {} 
-
-    for k, v in frequencies.items():
-
-        if k not in stop_words and type(k) == str:
-
-            d_clean[k] = v 
-
-    return d_clean  
-
-
-
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    "if type(frequencies) != dict or type(top_n) != int:
-
-        return () 
-
-    answer = () 
-
+def get_top_n(frequencies: dict, top_n: int):
+    if type(frequencies) != dict or type(top_n) != int or top_n <= 0 :
+        return ()
+    answer = ()
     if len(frequencies) < top_n:
-
-        print ('В словаре только', l, 'записей') 
-
+        print('В словаре только', len(frequencies), 'записей')
     freqsorted = sorted(frequencies.items(), key=lambda item: item[1], reverse = True)
-
-    d_cleansort = {} 
-
+    d_cleansort = {}
     for k, v in freqsorted:
-
-        d_cleansort[k] = v   
-
+        d_cleansort[k] = v
     m = 0
-
     for k, v in d_cleansort.items():
-
         if m != top_n:
-
-            answer += (k, ) 
-
+            answer += (k, )
         else:
-
             break
-
         m += 1
-
     return answer 
 
 
-python -m unittest discover -p "*_test.py" -s .
+
